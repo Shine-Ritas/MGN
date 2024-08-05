722ae0434f66b7a66bd3962a2da17ff6a709fe58
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-08-05 22:18:55 +0630

    [develop]animated hero carousel

--- src/pages/users/home/HeroCarousel.tsx
+++ src/pages/users/home/HeroCarousel.tsx
@@ -3,15 +3,15 @@ import {
   CarouselContent
 } from "@/components/ui/carousel"
 import useQuery from "@/hooks/useQuery";
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 import { Mogous } from "./types";
 import HeroCarouselCard from "./HeroCarouselCard";
-
+import Autoplay from "embla-carousel-autoplay"
 
 const HeroCarousel = () => {
 
   const { data, isLoading }  = useQuery(`users/carousel`);
-
+  
   // memoize the data 
   const memoData = useMemo(() => {
     console.log("memorized the carousel data")
@@ -19,7 +19,14 @@ const HeroCarousel = () => {
   }, [data]) as Mogous;
 
   return (
-    <Carousel className="min-w-full ">
+    <Carousel
+    plugins={[
+      Autoplay({
+        delay: 1800,
+      }),
+    ]}
+    className="min-w-full ">
+
       <CarouselContent className="-ml-1 gap-4">
         {!isLoading && memoData?.mogous.map((mogou) => (
           <HeroCarouselCard key={mogou.id} mogou={mogou} />
