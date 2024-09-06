d638e6cc61a8baa4bc4f113c03ccb70cd1d9ee21
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-09-07 00:11:57 +0630

    Dev: userRoute.tsx to use Redux store and lazy loading

--- src/route/userRoute.tsx
+++ src/route/userRoute.tsx
@@ -1,36 +1,43 @@
 import { lazy } from "react";
 import { AppRouteInterface } from "./type";
 import { userRouteCollection } from "@/constants/constants";
-import Show from "@/pages/users/Show/Show";
-import { UserLogin } from "@/pages/users/Auth/Login";
-
+import { userStore } from "@/redux/stores/userStore";
+import { Provider } from "react-redux";
 
 const UserLayout = lazy(() => import('@/layouts/UserLayout.tsx'));
 const HomePage = lazy(() => import('@/pages/users/home/Index.tsx'));
+const UserLogin = lazy(() => import('@/pages/users/Auth/Login.tsx'));
+const Show = lazy(() => import('@/pages/users/Show/Show.tsx'));
 
 
 export const userAuthenticatedRoutes: AppRouteInterface[] = [
   {
     path: userRouteCollection.home,
     element: (
-      <UserLayout >
-        <HomePage />
-      </UserLayout>
+      <Provider store={userStore}>
+        <UserLayout />
+      </Provider>
     ),
+    children: [
+      {
+        path: userRouteCollection.home,
+        element: (
+            <HomePage />
+        ),
+      },
+      {
+        path: userRouteCollection.show,
+        element: (
+            <Show />
+        ),
+      },
+      {
+        path: userRouteCollection.login,
+        element: (
+          <UserLogin />
+        )
+      }
+    ]
   },
-  {
-    path: userRouteCollection.show,
-    element: (
-      <UserLayout >
-        <Show />
-      </UserLayout>
-    ),
-  },
-  {
-    path : userRouteCollection.login,
-    element: (
-      <UserLogin />
-    )
-  }
 
 ];
