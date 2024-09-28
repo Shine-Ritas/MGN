48bfec3b03b7ed9a45786af5d5c577403126586a
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-09-28 18:12:53 +0630

    dev : style and hooks

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -3,5 +3,8 @@ import { UserAppDispatch, UserRootState } from './stores/userStore'
 import { AdminAppDispatch, AdminRootState } from './stores/adminStore'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AdminAppDispatch|UserAppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<AdminRootState | UserRootState> = useSelector
+export const useAppDispatch: () => AdminAppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<AdminRootState > = useSelector
+
+export const useUserAppDispatch: () => UserAppDispatch = useDispatch;
+export const useUserAppSelector: TypedUseSelectorHook<UserRootState> = useSelector;
